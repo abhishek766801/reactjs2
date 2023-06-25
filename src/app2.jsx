15526683456fb6d36825f687bcb5399f74c6cb19
abhisheksing420 <20518322-abhisheksing420@users.noreply.replit.com>
2023-06-25 10:47:49 +0000

    Initial commit

---
+++ src/app2.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+function app2() {
+  const [name, setName] = useState(null);
+  const [nameFieldText, setNameFieldText] = useState("gffggi");
+  const handleButtonClick = () => {
+    alert("hekko");
+  };
+  // Import CSS file
+  return (
+<>
+  
+</>
+  );
+}
+export default app2;
+
---
+++ src/components/ChildComponenet.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ChildComponent = (props) => {
+  return (
+    <div>
+      <h2>Child Component</h2>
+      <p>Name: {props.name}</p>
+      <p>Age: {props.age}</p>
+    </div>
+  );
+};
+
+export default ChildComponent;
